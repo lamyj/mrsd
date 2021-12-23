ac987471d6c739da7ef55e7184fb0f5115689d53
Julien Lamy <lamy@unistra.fr>
2021-12-23 14:59:10 +0100

    Add scaling gradient amplitudes

--- src/mrsd/__init__.py
+++ src/mrsd/__init__.py
@@ -1,1 +1,8 @@
+import numpy
+
 from .diagram import Diagram
+
+def scale_amplitudes(*args):
+    max_amplitude = numpy.max(numpy.abs([a for d,a in args]))
+    scaling = 1/max_amplitude
+    return [a * scaling for d,a in args]
